e0634091a97d521f35c6b12ca12c55384749fa19
HieutranEproject <116696372+HieutranEproject@users.noreply.github.com>
2022-11-22 05:54:51 +0000

    Update signup.js

--- JS/signup.js
+++ JS/signup.js
@@ -11,50 +11,12 @@ function signup(e){
         password: password,
     }
     var json= JSON.stringify(user);
-    localStorage.setItem(username,json);function signup(e){
-    event.preventDefault();
-
-    var email = document.getElementById('email').value;
-    var username =document.getElementById('username').value;
-    var password=document.getElementById('password').value;
-
-    var user= {
-        username : username,
-        email: email,
-        password: password,
-    }
-    var json= JSON.stringify(user);
     localStorage.setItem(username,json);
     alert("sign up successfully");
     window.location.href='../HTML/home.html'
 }
 
-function login(e){
-    event.preventDefault();
-    // var email = document.getElementById('email').value;
-  
-    var username=document.getElementById('username').value;
-    var password=document.getElementById('password').value;
 
-    var user=localStorage.getItem(username);
-    
-    var data=JSON.parse(user);
-
-    if(user==null){
-        alert("please enter your username and password")
-    }else if(username==data.username && password==data.password){
-        alert("login successfully");
-        sessionStorage.setItem("uname",username)
-        
-        document.getElementById("user-name").innerHTML=sessionStorage.uname;
-    }else{
-        alert("wrong password, please enter your password")
-    }
-
-}
-    alert("sign up successfully");
-    window.location.href='../HTML/home.html'
-}
 
 function login(e){
     event.preventDefault();
@@ -67,15 +29,19 @@ function login(e){
     
     var data=JSON.parse(user);
 
-    if(user==null){
-        alert("please enter your username and password")
-    }else if(username==data.username && password==data.password){
+    if(username=='' || password==''){
+        alert("please enter your username and password");
+    }
+    else if(username==data.username && password!=data.password){
+        alert("Wrong password, please check and enter your password again");
+    }
+    else if(username==data.username && password==data.password){
         alert("login successfully");
         sessionStorage.setItem("uname",username)
         
         document.getElementById("user-name").innerHTML=sessionStorage.uname;
-    }else{
-        alert("wrong password, please enter your password")
+    }else if(username!=data.username){
+        alert("Your account does not exist, please register or check your username and password");
     }
-
+     
 }
